b186cc45471b302a3e02570aa2f73c755f38d11d
5p3ktr0 <67437388+5p3ktr0@users.noreply.github.com>
2021-12-07 12:16:37 -0500

    changing default shell for all users and kali's zshrc file

--- setup.sh
+++ setup.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+
 #check if the user is root
-if [ "$EUID" -ne 0 ]
-then	
-	echo -e "\n You need to be root user or run with sudo \n" 
-	exit
-else
-	echo -e "\n You have proper permissions, continue\n"
-fi
-echo -e "\n [+] Downloading tmux, vim, python3, pip3, and net-tools"
-apt install tmux vim python3 python3-pip net-tools -y
+#if [ "$EUID" -ne 0 ]
+#then	
+#	echo -e "\n You need to be root user or run with sudo \n" 
+#	exit
+#else
+#	echo -e "\n You have proper permissions, continue\n"
+#fi
+#
+#echo -e "\n [+] Downloading tmux, vim, python3, pip3, zsh,  and net-tools"
+#apt install tmux vim python3 python3-pip net-tools zsh -y
+
+echo -e "\n [+] Changing shell to zsh as default shell for all users"
+for i in `getent passwd {1000..6000} | cut -d ":" -f1`
+do
+	echo $i
+	usermod -s /usr/bin/zsh $i
+done
+chsh -s /usr/bin/zsh
+
+
+
+
 
 
 
