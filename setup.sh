174d407bfd9129769507f632c0ff3088f69f07e0
5p3ktr0 <67437388+5p3ktr0@users.noreply.github.com>
2021-12-07 12:36:55 -0500

    Tmux configuration added

--- setup.sh
+++ setup.sh
@@ -11,15 +11,22 @@
 #fi
 #
 #echo -e "\n [+] Downloading tmux, vim, python3, pip3, zsh,  and net-tools"
-#apt install tmux vim python3 python3-pip net-tools zsh -y
+#apt install tmux xsel vim python3 python3-pip net-tools zsh -y
 
-echo -e "\n [+] Changing shell to zsh as default shell for all users"
-for i in `getent passwd {1000..6000} | cut -d ":" -f1`
-do
-	echo $i
-	usermod -s /usr/bin/zsh $i
-done
-chsh -s /usr/bin/zsh
+#echo -e "\n [+] Changing shell to zsh as default shell for all users"
+#for i in `getent passwd {1000..6000} | cut -d ":" -f1`
+#do
+#	echo $i
+#	usermod -s /usr/bin/zsh $i
+#done
+#chsh -s /usr/bin/zsh
+mv ./zshrc /etc/zshrc 
+
+echo -e "\n [+] Downloading Tmux Plugin Manager and tmux.conf file."
+git clone https://github.com/tmux-plugins/tpm /etc/tmux/plugins/tpm
+mv ./tmux.conf /etc/
+echo -e "\n [*] Tmux configuration completed. Bind key is now <C-a>. Install plugins using bind+I"
+
 
 
 
